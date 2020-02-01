23c66b514cba941a9d541dfad5480de75eaf0cce
saikat021 <51960068+saikat021@users.noreply.github.com>
2020-02-01 10:09:49 +0530

    All added

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var x=0;
+var speed=3;
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+ 
+  background(0);
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+  circle (x,200,100,100);
+
+ 
+    x=x+speed;
+  if ((x>=width-50)&&(speed==3))
+  {
+    speed=-3;
+  }
+  else if((x<=50)&&(speed==-3))
+  {
+                       speed=3;
+                       }
+
+ 
+    
+}
